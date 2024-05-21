4fa29e39b65577cd85fdc7105ea16c66c3f17471
sgy <1512362145@qq.com>
2024-05-21 16:54:45 +0800

    新建 Article 模型

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -1,13 +1,15 @@
 package controllers
 
 import (
-	"database/sql"
 	"fmt"
+	"goblog/app/models/article"
 	"goblog/pkg/logger"
 	"goblog/pkg/route"
 	"goblog/pkg/types"
 	"net/http"
 	"text/template"
+
+	"gorm.io/gorm"
 )
 
 // ArticlesController 文章相关页面
@@ -19,18 +21,18 @@ func (*ArticlesController) Show(w http.ResponseWriter, r *http.Request) {
 	//1. 获取 URL 参数
 	// vars := mux.Vars(r) // 从HTTP请求中获取路由参数的值
 	// id := vars["id"]
-	id := getRouteVariable("id", r)
+	id := route.GetRouteVariable("id", r)
 
 	//2. 读取对应的文章数据
 	// article := Article{}
 	// query := "SELECT * FROM articles WHERE id = ?"
 	// err := db.QueryRow(query, id).Scan(&article.ID, &article.Title, &article.Body)
 	//QueryRow() 来读取单条数据；Scan() 将查询结果赋值到我们的 article struct 中，传参应与数据表字段的顺序保持一致。
-	article, err := getArticleByID(id)
+	article, err := article.Get(id)
 
 	//3 如果出现错误
 	if err != nil {
-		if err == sql.ErrNoRows { //Scan() 发现没有返回数据的话，会返回 sql.ErrNoRows 类型的错误
+		if err == gorm.ErrRecordNotFound { // 此处是 gorm.ErrRecordNotFound,不是sql.ErrNoRows
 			//3.1 数据未找到
 			w.WriteHeader(404) //http.StatusNotFound
 			fmt.Fprint(w, "404 文章未找到")
@@ -44,8 +46,8 @@ func (*ArticlesController) Show(w http.ResponseWriter, r *http.Request) {
 		//4. 读取成功
 		// tmpl, err := template.ParseFiles("resources/views/articles/show.gohtml") // 加载模板文件show.gohtml,后缀名也可以是,tmpl
 		tmpl, err := template.New("show.gohtml").Funcs(template.FuncMap{
-			"RouteName2URL": route.Name2URL,
-			"Int64ToString": types.Int64ToString,
+			"RouteName2URL":  route.Name2URL,
+			"Uint64ToString": types.Uint64ToString,
 		}).ParseFiles("resources/views/articles/show.gohtml")
 
 		logger.LogError(err)
