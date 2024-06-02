9a8c121b380e704704608418e5c0785406c5f53f
sgy <1512362145@qq.com>
2024-06-03 05:59:07 +0800

    配置和环境变量

--- main.go
+++ main.go
@@ -3,25 +3,22 @@ package main
 import (
 	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
-	"goblog/pkg/logger"
+	"goblog/config"
+	c "goblog/pkg/config"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
-var router *mux.Router
-
-// Article 对应一条文章数据
-type Article struct {
-	Title, Body string
-	ID          int64
+func init() {
+	// 初始化配置信息
+	config.Initialize()
 }
 
 func main() {
-
+	// 初始化 SQL
 	bootstrap.SetupDB()
-	router = bootstrap.SetupRoute()
 
-	err := http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
-	logger.LogError(err)
+	// 初始化路由绑定
+	router := bootstrap.SetupRoute()
+
+	http.ListenAndServe(":"+c.GetString("app.port"), middlewares.RemoveTrailingSlash(router))
 }
