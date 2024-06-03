139ad6b46298f4ebf66245bc170084c9ea09fed2
sgy <1512362145@qq.com>
2024-06-03 11:57:44 +0800

    暂完工!

--- pkg/session/session.go
+++ pkg/session/session.go
@@ -2,7 +2,6 @@
 package session
 
 import (
-	"goblog/pkg/config"
 	"goblog/pkg/logger"
 	"net/http"
 
@@ -10,7 +9,7 @@ import (
 )
 
 // Store gorilla sessions 的存储库
-var Store = sessions.NewCookieStore([]byte(config.GetString("app.key")))
+var Store = sessions.NewCookieStore([]byte("33446a9dcf9ea060a0a6532b166da32f304af0de"))
 
 // Session 当前会话
 var Session *sessions.Session
@@ -27,7 +26,7 @@ func StartSession(w http.ResponseWriter, r *http.Request) {
 
 	// Store.Get() 的第二个参数是 Cookie 的名称
 	// gorilla/sessions 支持多会话，本项目我们只使用单一会话即可
-	Session, err = Store.Get(r, config.GetString("session.session_name"))
+	Session, err = Store.Get(r, "goblog-session")
 	logger.LogError(err)
 
 	Request = r
